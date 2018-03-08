ee8201d936989eb4264a0e2de623c374e27b9e70
Dorian Stoll <dorian.stoll@tmsp.io>
2018-03-08 22:39:49 +0100

    First version

---
+++ src/BinaryReaderPatch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using FinePrint;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace KSPNET4
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class BinaryReaderPatch : MonoBehaviour
+    {
+        void Awake()
+        {
+            // Fetch the original method
+            MethodInfo brpc = typeof(BinaryReader).GetMethod("PeekChar", BindingFlags.Instance | BindingFlags.Public);
+            
+            // Fetch our replacement
+            MethodInfo ftpc = typeof(FakeReader).GetMethod("PeekChar", BindingFlags.Instance | BindingFlags.Public);
+            
+            // Redirect the original method to the new one
+            Detourer.TryDetourFromTo(brpc, ftpc);
+        }
+    }
+
+    /// <summary>
+    /// A dummy type extending BinaryReader so we can create a matching method signature
+    /// </summary>
+    public class FakeReader : BinaryReader
+    {
+        public FakeReader([NotNull] Stream input) : base(input)
+        {
+        }
+
+        public FakeReader([NotNull] Stream input, [NotNull] Encoding encoding) : base(input, encoding)
+        {
+        }
+
+        public FakeReader(Stream input, Encoding encoding, Boolean leaveOpen) : base(input, encoding, leaveOpen)
+        {
+        }
+
+        public override Int32 PeekChar()
+        {
+            if (BaseStream == null)
+                throw new ObjectDisposedException(null, "Cannot access a closed file.");
+            if (!BaseStream.CanSeek)
+                return -1;
+            if (BaseStream.Position >= BaseStream.Length) 
+                return -1;
+            
+            Int64 pos = BaseStream.Position;
+            Int32 ret = ReadChar();
+            BaseStream.Position = pos;
+            return ret;
+        }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KSP-NET4")]
+[assembly: AssemblyDescription("A patch that fixes bugs occuring when running KSP using the new unity mono / .NET 4.6")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KSP-NET4")]
+[assembly: AssemblyCopyright("Copyright © Dorian Stoll 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("E3AE62F0-E8CF-4153-916C-305B5187CD2C")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
